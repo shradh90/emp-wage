6fd7c06d229b184c71d23bd8d9b9b0ad7cdeede6
shradh90 <shradhad1996@gmail.com>
2021-06-09 19:03:41 +0530

    employee salary of a month

--- empwage.java
+++ empwage.java
@@ -1,3 +1,70 @@
+
+
+
+
+
+
+public class empwage {
+	public static void main(String[] args) {
+		int is_full_time = 2;
+		int is_part_time = 1;
+		int emprateperhr = 20;
+       		int sum = 0;
+		for(int i = 0; i <= 30; i++) {
+
+			double empcheck = Math.floor(Math.random() * 10 ) % 3;
+			if(empcheck == is_full_time)
+ 			{
+				int emphrs = 8;
+				int salary = (emphrs * emprateperhr);
+                           	sum = sum + salary;
+				System.out.println("salary = " + salary);
+			}
+			else if(empcheck == is_part_time)
+			{
+				int emphrs = 4;
+                        	int salary = (emphrs * emprateperhr);
+				sum = sum + salary;
+				System.out.println("salary = " + salary);
+			}
+			else 
+			{
+			int salary = 0;
+			sum =sum + salary;
+                        System.out.println("salary = " + salary);
+			}
+		}
+ 	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class empwage {
 	public static void main(String[] args) {
 		int is_full_time = 2;
