d9e800e02368e4eba38b2d44cc745c9b0d7e50a6
shradh90 <shradhad1996@gmail.com>
2021-06-09 18:41:10 +0530

    using switch case calculating the salary

--- empwage.java
+++ empwage.java
@@ -3,25 +3,31 @@ public class empwage {
 		int is_full_time = 2;
 		int is_part_time = 1;
 		int emprateperhr = 20;
-		double empcheck = Math.floor(Math.random() * 10 ) % 3;
-		if(empcheck == is_full_time)
- 		{
-			int emphrs = 8;
-			int salary = (emphrs * emprateperhr);
-			System.out.println("salary = " + salary);
-		}
-		else if(empcheck == is_part_time)
-		{
-			int emphrs = 4;
-                        int salary = (emphrs * emprateperhr);
-			System.out.println("salary = " + salary);
-		}
-		else 
-		{
-			int salary = 0;
-                        System.out.println("salary = " + salary);
+		int empcheck = (int) (Math.floor(Math.random() * 10 )) % 3;
+		int emphrs;
+		int salary;
+		switch (empcheck) {
+			  case 0:
+			     	emphrs = 0;
+                             	break;
+		 	  case 1:
+			     	emphrs = 4;
+                             	break; 		 
+ 		          case 2:
+			     	emphrs = 8;
+				break;
+     		}
+ 		salary = (emphrs * emprateperhr);
+                System.out.println("salary = " + salary);
 
-		}
-	}
-
+ 	}
 }
+
+
+
+
+
+
+
+
+
