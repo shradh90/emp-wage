c1e5cc0e4fbc4be8d858643b20aae9d79c1e4cd2
shradh90 <shradhad1996@gmail.com>
2021-06-09 16:06:00 +0530

    daily employee wage

--- empwage.java
+++ empwage.java
@@ -3,9 +3,19 @@ public class empwage {
 		int is_full_time = 1;
 		double empcheck = Math.floor(Math.random() * 10 ) % 2;
 		if(empcheck == is_full_time)
-		System.out.println("employee is present");
+ 		{
+			System.out.println("employee is present");
+			int emprateperhr = 20;
+			int emphrs = 8;
+			int salary = (emphrs * emprateperhr);
+			System.out.println("salary = " + salary);
+		}
 		else
-		System.out.println("employee is absent");
+		{
+			System.out.println("employee is absent");
+			int salary = 0;
+			System.out.println("salary = " + salary);
+		}
 	}
 
 }
