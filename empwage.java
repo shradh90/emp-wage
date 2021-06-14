51963de50c5641fc48bb1ff93093f9055761d735
shradh90 <84696361+shradh90@users.noreply.github.com>
2021-06-14 09:43:14 +0530

    Update empwage.java

--- empwage.java
+++ empwage.java
@@ -1,28 +1,35 @@
-public class empwage {
+package java1;
+
+public class java1 {
 	public static void main(String[] args) {
 		int is_full_time = 2;
 		int is_part_time = 1;
 		int emprateperhr = 20;
-		int empcheck = (int) (Math.floor(Math.random() * 10 )) % 3;
-		int emphrs = 0;
-		int salary;
-		switch (empcheck) {
+       		int sum = 0;
+       		int emphrs = 0;
+		for(int i = 0; i < 30; i++) {
+
+			int empcheck = (int) (Math.floor(Math.random() * 10 ) % 3);
+			switch (empcheck) {
 			  case 0:
 			     	emphrs = 0;
-                             	break;
+                           	break;
 		 	  case 1:
 			     	emphrs = 4;
-                             	break; 		 
- 		          case 2:
+                           	break; 		 
+		          case 2:
 			     	emphrs = 8;
 				break;
-     		}
- 		salary = (emphrs * emprateperhr);
-                System.out.println("salary = " + salary);
-
+			}
+			int salary = (emphrs * emprateperhr);
+			sum =sum + salary; 
+                        
+		}
+		System.out.println("toyal salary = " + sum);
+		
  	}
+	
 }
-
 
 
 
