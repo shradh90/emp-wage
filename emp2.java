c2d8d5a24a0db13d93f3c8cbfcd856a2e8e26a2e
shradh90 <shradhad1996@gmail.com>
2021-06-15 18:06:09 +0530

    by using oops concept calculating daily wage

---
+++ emp2.java
@@ -0,0 +1,36 @@
+package emp;
+/* calculate the daily Employee Wage 
+ * Working Hour Full Time: 8
+ * Wage per Hour : 20
+ * @author: Shradha Desai
+ * @since: 14/06/2021
+ */
+
+public class EmpWage {
+	//constant variables
+	public static final int IS_FULL_TIME = 1;
+	public static final int empwagePerHour = 20;	
+	private static void EmployeeWage() {
+		//variable
+		int emphrs = 8;
+		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		//To calculate the daily salary of employee
+		if(empCheck == IS_FULL_TIME)
+ 		{
+			System.out.println("employee is present");
+			int salary = (emphrs * empwagePerHour);
+			System.out.println("salary = " + salary);
+		}
+		else
+		{
+			System.out.println("employee is absent");
+			int salary = 0;
+			System.out.println("salary = " + salary);
+		}
+		
+	}
+	public static void main(String[] args) {
+		EmployeeWage();
+	}
+	
+	}
