fe9c2b3769acc28952e86b693738f1683d2e7c34
shradh90 <84696361+shradh90@users.noreply.github.com>
2021-06-16 08:42:04 +0530

    Update emp1.java

--- emp1.java
+++ emp1.java
@@ -9,7 +9,8 @@ public class EmpWage {
 	private static void Attendence() {
 		//To Check whether employee is present or absent
 		double empCheck = Math.floor(Math.random() * 10 ) % 2;
-		if(empCheck == IS_FULL_TIME) {
+		// if condition will check empCheck is equal to IS_FULL_TIME and check whether employee is present or not
+		if(empCheck == IS_FULL_TIME) { 
 		System.out.println("Employee is Present");
 		} else {
 		System.out.println("Employee is Absent");
